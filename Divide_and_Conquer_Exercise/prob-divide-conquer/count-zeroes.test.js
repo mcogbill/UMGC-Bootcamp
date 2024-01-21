20caed394a09a22d68ced805f7710bffb2bc2482
mcogbill <malcolm.cogbill@gmail.com>
2024-01-21 11:24:30 -0500

    Completed Divide and Conquer Exercise

--- Divide_and_Conquer_Exercise/prob-divide-conquer/count-zeroes.test.js
+++ Divide_and_Conquer_Exercise/prob-divide-conquer/count-zeroes.test.js
@@ -1,7 +1,7 @@
 const countZeroes = require("./count-zeroes")
 
-describe("#countZeroes", function(){
-  it("counts the number of zeroes", function(){
+describe("#countZeroes", function () {
+  it("counts the number of zeroes", function () {
     expect(countZeroes([1, 1, 1, 1, 0, 0])).toBe(2)
     expect(countZeroes([1, 0, 0, 0, 0])).toBe(4)
     expect(countZeroes([0, 0, 0])).toBe(3)
